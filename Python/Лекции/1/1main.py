fd4087330b6005b5d20435ded7a7e0e16d9e5040
Timur_2101 <timurka260197@gmil.com>
2023-10-11 18:15:33 +0200

    1

---
+++ Python/Лекции/1/1main.py
@@ -0,0 +1,15 @@
+# # print(5, 8, 6)
+# n = 5
+# a = 5.8
+# c = "'cnhjrf'"
+# print ("{}-{}-{}".format(n,a,c))
+
+# a = int(input("Введите первое число :"))
+
+# b = int(input("Введите второе число :"))
+# print (a, "+", b , "=" ,a+b)
+
+
+a = 5.8898
+b = 2.2342
+print(round(a*b, 3))
---
+++ Python/Лекции/2/2.py
@@ -0,0 +1,2 @@
+mas = [(i,i*i) for i in range(1, 100) if i % 2 == 0]
+print(mas)
