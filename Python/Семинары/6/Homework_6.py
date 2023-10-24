3b45a90980855bda62b8e5181dd7c0c08ca9dd24
Timur_2101 <timurka260197@gmil.com>
2023-10-24 15:29:47 +0200

    семинар функции высшего порядка

--- Python/Семинары/6/Homework_6.py
+++ Python/Семинары/6/Homework_6.py
@@ -1,7 +1,4 @@
-# # Определить индексы элементов массива (списка),
-# # значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума).
-# # На вход подается список с элементамиlist_1 и границы диапазона в виде чисел min_number, max_number.
-# #
+# # ОЁ
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 min_number = 0
 max_number = 10
@@ -14,9 +11,33 @@ for i in range(0, len(list_1)):
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 
 
-a1 = 5
-d = -1
-n = 10
-an = a1 + (n - 1) * d
-for i in range(a1, a1 + n  * d, d):
-    print(i)
+# a1 = 5
+# d = -1
+# n = 10
+# an = a1 + (n - 1) * d
+# for i in range(a1, a1 + n  * d, d):
+#     print(i)
+
+# a1 = 2
+# d = 3
+# n = 4
+# res_p = []
+# def progress(a1, d, n):
+#     if n == 0:
+#         return ""
+#     return progress(a1, d, n - 1) + str(a1 + (n-1) * d)
+#
+# print(f"{progress(2,3,4)}")
+
+def func(min_number, max_number, i):
+    if (i <= len(list_1)-1):
+        if (max_number >= list_1[i] >= min_number):
+            print(i)
+            func(min_number, max_number, i+1)
+        else:
+            func(min_number, max_number, i+1)
+    else:
+        return
+func(min_number, max_number, 0)
+
+
---
+++ Python/Семинары/7/funkcii_vischego_pirydka.py
@@ -0,0 +1,55 @@
+# def sum(x):
+#     return x
+#
+#
+# print(sum(5))
+#
+# sum1 = lambda x: x
+# print(sum1(6))
+#
+# print((lambda x: x)(8))
+#
+# number = "12345"
+# number = list(number)
+# print(number)
+#
+# number = list(map(int, number))
+# print(number)
+#
+# number = list(map(lambda number: number * 2, number))
+# print(number)
+#
+# res = list(filter(lambda x: x % 4 == 0, number))
+# print(res)
+
+# У вас есть код, который вы не можете менять (так часто бывает, когда код в глубине программы используется множество раз и вы не хотите ничего сломать):
+# transformation = <???>
+# values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+# transormed_values = list(map(transformation, values))
+# Единственный способ вашего взаимодействия с этим кодом - посредством задания функции transformation.
+# Однако вы поняли, что для вашей текущей задачи вам не нужно никак преобразовывать список значений, а нужно получить его как есть.
+# Напишите такое лямбда-выражение transformation, чтобы transformed_values получился копией values.
+# Пример ввода и вывода данных представлены на следующем слайде
+# values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+# transformation = list(map(lambda values: values, values))
+# print(transformation)
+# if values == transformation:
+#     print("ok")
+# else:
+#     print("fail")
+
+# ////////////////////////
+# Напишите функцию same_by(characteristic, objects), которая проверяет, все ли объекты имеют одинаковое значение некоторой характеристики,
+# и возвращают True, если это так. Если значение характеристики для разных объектов отличается - то False. Для пустого набора объектов,
+# функция должна возвращать True. Аргумент characteristic - это функция, которая принимает объект и вычисляет его характеристику.
+
+values = [0, 2, 10, 6]
+
+
+def same_by(characteristic, objects):
+    # return len(objects) == len(list(filter(characteristic, objects)))
+    return len(list(filter(characteristic, objects))) == 0
+if same_by(lambda x: x % 2 == 0, values):
+    print('same')
+else:
+    print('different')
