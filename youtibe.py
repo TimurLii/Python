74e063945264e3d17f47e326ce8b6e02e15dbd4d
Timur_2101 <timurka260197@gmil.com>
2023-10-25 13:10:04 +0200

    add leetcode.

--- youtibe.py
+++ youtibe.py
@@ -1,7 +1,24 @@
-strs = ["flower", "flow", "flight"]
-ans = ""
-for i in strs:
-    for j in i:
-        if j == i:
-            ans += i
-    print(ans)
+# strs = ["flower", "flow", "flight"]
+# ans = ""
+# for i in strs:
+#     for j in i:
+#         if j == i:
+#             ans += i
+#
+# print(ans)
+
+# d = dict.fromkeys([1,2,3,4,5], "values")
+# print(d)
+d1 = dict([[6, 2], [8, 9]])
+d2 = dict([[1, 2], [3, 4], [5, 6]])
+# print(d2)
+# print(d2[3])
+#
+# print(d2.items())
+# print(d2.keys())
+# print(d2.values())
+# d1.update(d2)
+# print(d1)
+
+t = d1.pop(6)
+print(t, d1)
